fc41b7ae20cd02e7457a360ec116fc4c729b7b9d
Stefanie Sluschny <s.sluschny@web.de>
2025-04-10 12:54:10 +0200

    add input-abfrage für Laufwerke

--- duplikate_programm.ps1
+++ duplikate_programm.ps1
@@ -15,9 +15,35 @@ foreach($drive in $drives) {
     write-host $drive
 }
 
-write-host "Welche Laufwerke wollen Sie untersuchen?"
 
 # Usereingabe der gewünschten Laufwerke
-read-host
+$drivesToCheck = read-host "Welche Laufwerke wollen Sie untersuchen?`n Eingabe 'alle' oder einzelne Laufwerke, getrennt mit Komma"
 
-# mit der restlichen Sammlung von Laufwerken (Schleife)
+# Auswerten der Usereingabe
+
+if($drivesToCheck -eq "alle") {
+
+    # alle: auf jedem Laufwerk suchen
+    $drivesToCheck = $drives
+
+} else {
+
+    # ein oder mehrere Laufwerke // c c,e c,d,e c,
+    $array = $drivesToCheck -split ','
+    
+    #selectedDrives wird leeres Array
+    $selectedDrives = @()
+
+    foreach($letter in $array) {
+        $drive = Get-PSDrive -Name $letter
+        write-host "Add $($drive.Root)"
+        $selectedDrives += $drive
+    }
+    $drivesToCheck = $selectedDrives
+}
+
+# falsche Angabe
+
+foreach($drive in $drivesToCheck) {
+    Write-Host "Untersuche $($drive.Root) `n"
+}
